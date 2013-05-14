7fdb30e0e490787994126cc1506e46e18061a775
jtokarski <jk.tokarski@jtsi.pl>
2013-05-14 22:16:42 +0200

    Changed default application name "myApp" to project specific, "carPoolingApp"

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('carPoolingApp', ['carPoolingApp.filters', 'carPoolingApp.services', 'carPoolingApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider.when('/', {templateUrl: 'partials/dashboard.html', controller: DashboardController});
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,7 +2,7 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
+angular.module('carPoolingApp.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
